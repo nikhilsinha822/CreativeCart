13040faf9f83e64cc0fec0519b508a655e18d899
nikhilsinha822 <nikhilsinha822@gmail.com>
2024-05-29 19:03:02 +0530

    Suspense wrap for user routes

--- app/(main)/(user)/layout.tsx
+++ app/(main)/(user)/layout.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Navbar from '@/app/ui/Navbar'
 import Footer from '@/app/ui/Footer'
+import { Suspense } from 'react'
+import Loading from '@/app/ui/Loading'
 
 interface HomeLayoutProps {
   children?: React.ReactNode
@@ -10,9 +12,11 @@ export const revalidate = 3600
 const HomeLayout = ({ children }: HomeLayoutProps) => {
   return (
     <div>
-        <Navbar />
+      <Navbar />
+      <Suspense fallback={<Loading />}>
         {children}
-        <Footer />
+      </Suspense>
+      <Footer />
     </div>
   )
 }
